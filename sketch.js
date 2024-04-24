c8045875a0e9652934c22f1f8f6030b3e88d5881
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2024-04-24 18:06:44 +0200

    final

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var radius;
+var ang;
+
+var angleOffset;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  stroke(255);
+  angleMode(DEGREES);
+  noFill();
+  textSize(20);
+
+  radius = 500;
+  ang = 90;
+  angleOffset = 0;
+}
+
+function draw() {
+  angleOffset += 0.1; // Increment the angle to create movement
+
+  background(195, 100, 98); // Color-changing background with transparency
+  translate(width / 2, height / 2);
+  beginShape();
+  for (ang = 0; ang <= 360; ang += 1) {
+    var offsetXRad = cos(ang + angleOffset) + 1;
+    var offsetYRad = sin(ang + angleOffset) + 1;
+    var x = radius * cos(angleOffset * offsetXRad) * cos(ang);
+    var y = radius * cos(angleOffset * offsetYRad) * sin(ang);
+    stroke(255, map(sin(ang + angleOffset), -1, 1, 255, 0));
+    line(0, 0, x, y); // Lines originate from the center
+    circle(x, y, 5);
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+}
