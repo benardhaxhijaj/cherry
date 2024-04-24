1dae565c51f93cb8454a320c131c6f8614432e3e
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2024-04-24 18:27:17 +0200

    fin

--- sketch.js
+++ sketch.js
@@ -29,7 +29,11 @@ function draw() {
     var y = radius * cos(angleOffset * offsetYRad) * sin(ang);
     stroke(255, map(sin(ang + angleOffset), -1, 1, 255, 0));
     line(0, 0, x, y); // Lines originate from the center
-    circle(x, y, 5);
+
+    fill(255); // Set fill color to white
+    circle(x, y, 5); // Draw filled circle
+    noFill(); // Disable fill for other shapes
+
     vertex(x, y);
   }
   endShape(CLOSE);
